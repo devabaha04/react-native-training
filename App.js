cfa06d8492c7937cfe36514ef4574cd9f43609a3
Dev Abaha 04 <devabaha04@gmail.com>
2022-05-26 11:37:38 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  StatusBar,
+} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView></SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
---
+++ README.md
@@ -0,0 +1,21 @@
+ # React Native Training
+ 
+- This is a training area for ABAHA newbie(s).
+
+
+## WHO AM I?
+
+- I'm a collection man with full of exercise for you.
+
+
+## HOW TO PLAY WITH ME?
+
+- Checkout the branch of any topic that you need, set your new branch name with format: 
+`[EXERCISE_ID]/[YOUR_DEV_NAME]` ([YOUR_DEV_NAME] is lowercase).
+ex: prac1/minhnh
+- Play with it, push it.
+
+
+
+
+....have fun & good luck!!
